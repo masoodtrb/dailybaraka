744d0d2d497be00305d4f5af283229336de8fbc9
Mehdi Rad <mahdirad0@gmail.com>
2019-06-01 18:15:45 +0430

    #DB-I333 #status Done #time 2h

--- pages/search.js
+++ pages/search.js
@@ -32,12 +32,13 @@ class Search extends Component {
 
   render() {
     const { products, searchValue } = this.props;
+    const { locale } = this.props.intl;
     return (
       <div>
         <Head title={this.props.intl.formatMessage(messages.title)} />
         <Nav />
 
-        <div className="page sectors">
+        <div className="page search">
           <div className="container">
             <h1>
               <FormattedMessage
@@ -46,59 +47,77 @@ class Search extends Component {
                 defaultMessage="Search result for '{searchValue}'"
               />
             </h1>
-
-            <div className="columns is-multiline">
-              {products.result && products.result.length > 0 ? (
-                products.result.map(product => (
-                  <div
-                    key={"search-product" + product.id}
-                    className="column is-4"
-                  >
-                    <Link
-                      href={`/product?slug=${product.slug}&lang=${
-                        this.props.intl.locale
-                      }`}
-                      as={`/${this.props.intl.locale}/product/${product.slug}`}
-                    >
-                      <a>
-                        <div className="box">
-                          <article className="media">
-                            <div className="media-left">
-                              <figure className="image">
-                                <img
-                                  src={
-                                    product.mainPicture
-                                      ? process.env.API_URL +
-                                        "/api/shop/general/v1/file/" +
-                                        product.mainPicture.id
-                                      : "/static/images/128x128.png"
-                                  }
-                                  alt={product.name}
-                                />
-                              </figure>
-                            </div>
-                            <div className="media-content">
-                              <div className="content">
-                                <p>
-                                  <strong>{product.name}</strong>
-                                </p>
-                              </div>
-                            </div>
-                          </article>
-                        </div>
-                      </a>
-                    </Link>
+            <hr />
+            {products.result && products.result.length > 0 ? (
+              products.result.map(product => (
+                <div
+                  key={"search-product" + product.id}
+                  className="search__product"
+                >
+                  <div className="product__content">
+                    <img
+                      src={
+                        product.mainPicture
+                          ? process.env.API_URL +
+                            "/api/shop/general/v1/file/" +
+                            product.mainPicture.id
+                          : "/static/images/128x128.png"
+                      }
+                      alt={product.name}
+                    />
+                    <div>
+                      <Link
+                        href={`/product?slug=${product.slug}&lang=${locale}`}
+                        as={`/${locale}/product/${product.slug}`}
+                      >
+                        <a>
+                          <h3>{product.name}</h3>
+                        </a>
+                      </Link>
+                      <p>{product.description}</p>
+                    </div>
                   </div>
-                ))
-              ) : (
-                <div className="notification column is-12">
-                  <FormattedMessage
-                    id="search.empty"
-                    defaultMessage="No result is found!"
-                  />
+                  <div className="product__shortcut">
+                    <span className="shortcut__supplier">
+                      More products from&nbsp;
+                      <Link
+                        href={`/supplier/?id=${product.supplier.id}&name=${
+                          product.supplier.name
+                        }&lang=${locale}`}
+                        as={`/${locale}/supplier/${product.supplier.id}/${
+                          product.supplier.name
+                        }`}
+                      >
+                        <a>{product.supplier.name}</a>
+                      </Link>
+                    </span>
+                    <a href="#" className="shortcut__organization">
+                      <Link
+                        href={`/supplier/?id=${product.supplier.id}&name=${
+                          product.supplier.name
+                        }&lang=${locale}`}
+                        as={`/${locale}/supplier/${product.supplier.id}/${
+                          product.supplier.name
+                        }`}
+                      >
+                        <a>View accreditation organization</a>
+                      </Link>
+                    </a>
+                    <a href="#" className="shortcut__button">
+                      <i class="fas fa-plus-circle" />
+                      &nbsp;Add to shopping list
+                    </a>
+                  </div>
                 </div>
-              )}
-            </div>
+              ))
+            ) : (
+              <div className="notification column is-12">
+                <FormattedMessage
+                  id="search.empty"
+                  defaultMessage="No result is found!"
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
