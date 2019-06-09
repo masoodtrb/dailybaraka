08b9bf52a16b27fef3e6b8dde09a69edfbb21313
Mehdi Rad <mahdirad0@gmail.com>
2019-06-09 14:09:41 +0430

    #DB-I373 #status Done #time 45m

--- pages/sector.js
+++ pages/sector.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Link from "next/link";
+import { FormattedMessage } from "react-intl";
 
 import * as sectorService from "../services/sector";
 
@@ -13,7 +14,10 @@ class Sector extends Component {
   static async getInitialProps({ query }) {
     return {
       sector: await sectorService.get(query.lang, query.id),
-      suppliers: await sectorService.getSuppliersAndProducts(query.lang, query.id)
+      suppliers: await sectorService.getSuppliersAndProducts(
+        query.lang,
+        query.id
+      )
     };
   }
 
@@ -29,74 +33,88 @@ class Sector extends Component {
             <h1>{sector.name}</h1>
           </div>
 
-          {suppliers && suppliers.result && suppliers.result.map(supplier => (
-            <div key={"supplier-" + supplier.id} className="sector__supplier">
-              <div className="container">
-                <Link
-                  href={`/supplier/?id=${supplier.id}&name=${supplier.name}&lang=${this.props.intl.locale}`}
-                  as={`/${this.props.intl.locale}/supplier/${supplier.id}/${
-                    supplier.name
-                  }`}
-                >
-                  <a>
-                    <h2>
-                      <i className="fas fa-store" />
-                      {supplier.name}
-                    </h2>
-                  </a>
-                </Link>
-                <div className="sector__products">
-                  <div className="columns is-multiline">
-                    {supplier.products.map(product => (
-                      <div
-                        key={"product-" + product.id}
-                        className="column is-2"
-                      >
-                        <Link
-                          href={`/product?slug=${product.slug}&lang=${this.props.intl.locale}`}
-                          as={`/${this.props.intl.locale}/product/${product.slug}`}
-                        >
-                          <a>
-                            <div className="card">
-                              <div className="card-image">
-                                <figure className="image is-4by3">
-                                  <img
-                                    src={
-                                      product.mainPicture
-                                        ? process.env.API_URL +
-                                          "/api/shop/general/v1/file/" +
-                                          product.mainPicture.id
-                                        : "/static/images/128x128.png"
-                                    }
-                                    alt={product.name}
-                                  />
-                                </figure>
-                              </div>
-                              <div className="card-content">
-                                <h3>{product.name}</h3>
-                              </div>
-                            </div>
-                          </a>
-                        </Link>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-                <div className="sector__more">
+          {suppliers &&
+            suppliers.result &&
+            suppliers.result.map(supplier => (
+              <div key={"supplier-" + supplier.id} className="sector__supplier">
+                <div className="container">
                   <Link
-                    href={`/supplier/?id=${supplier.id}&name=${supplier.name}&lang=${this.props.intl.locale}`}
+                    href={`/supplier/?id=${supplier.id}&name=${
+                      supplier.name
+                    }&lang=${this.props.intl.locale}`}
                     as={`/${this.props.intl.locale}/supplier/${supplier.id}/${
                       supplier.name
                     }`}
                   >
                     <a>
-                      More...
+                      <h2>
+                        <i className="fas fa-store" />
+                        {supplier.name}
+                      </h2>
                     </a>
                   </Link>
+                  <Link
+                    href={`/supplier/?id=${supplier.id}&name=${
+                      supplier.name
+                    }&lang=${this.props.intl.locale}`}
+                    as={`/${this.props.intl.locale}/supplier/${supplier.id}/${
+                      supplier.name
+                    }`}
+                  >
+                    <a className="sector__more">
+                      <FormattedMessage
+                        id="sector.supplier.link"
+                        defaultMessage="More..."
+                      />
+                    </a>
+                  </Link>
+                  <div className="sector__products">
+                    <div className="columns is-multiline">
+                      {supplier.products.map(
+                        (product, index) =>
+                          index < 6 && (
+                            <div
+                              key={"product-" + product.id}
+                              className="column is-2"
+                            >
+                              <Link
+                                href={`/product?slug=${product.slug}&lang=${
+                                  this.props.intl.locale
+                                }`}
+                                as={`/${this.props.intl.locale}/product/${
+                                  product.slug
+                                }`}
+                              >
+                                <a>
+                                  <div className="card">
+                                    <div className="card-image">
+                                      <figure className="image is-4by3">
+                                        <img
+                                          src={
+                                            product.mainPicture
+                                              ? process.env.API_URL +
+                                                "/api/shop/general/v1/file/" +
+                                                product.mainPicture.id
+                                              : "/static/images/128x128.png"
+                                          }
+                                          alt={product.name}
+                                        />
+                                      </figure>
+                                    </div>
+                                    <div className="card-content">
+                                      <h3>{product.name}</h3>
+                                    </div>
+                                  </div>
+                                </a>
+                              </Link>
+                            </div>
+                          )
+                      )}
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
 
           {suppliers && suppliers.pagination && suppliers.pagination.page > 1 && (
             <nav
